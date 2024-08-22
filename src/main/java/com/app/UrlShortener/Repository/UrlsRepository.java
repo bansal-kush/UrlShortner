6eaba21297b0ee7ee17a2acccd8e5b67308fc374
kb150802 <bansalkushagra150802@gmail.com>
2024-08-22 21:25:46 +0530

    added shortner

--- src/main/java/com/app/UrlShortener/Repository/UrlsRepository.java
+++ src/main/java/com/app/UrlShortener/Repository/UrlsRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UrlsRepository extends JpaRepository<ShortenedUrl, Integer> {
+    public ShortenedUrl findByShortUrl(String shortUrl);
 }
--- src/main/java/com/app/UrlShortener/service/UrlService.java
+++ src/main/java/com/app/UrlShortener/service/UrlService.java
@@ -2,6 +2,7 @@ package com.app.UrlShortener.service;
 
 import com.app.UrlShortener.Repository.UrlsRepository;
 import com.app.UrlShortener.Repository.UserRepository;
+
 import com.app.UrlShortener.model.ShortenUrlRequest;
 import com.app.UrlShortener.model.ShortenUrlResponse;
 import com.app.UrlShortener.model.ShortenedUrl;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class UrlService {
@@ -19,11 +21,18 @@ public class UrlService {
     private UrlsRepository urlsRepository;
     @Autowired
     private UserRepository userRepository;
+
+    private static final int NUM_CHARS_SHORT_LINK = 7;
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static  final String BASE_URL = "http://localhost:8080/";
+
+    private final Random random = new Random();
+
     public ResponseEntity<?> createShortenedUrl(String username, ShortenUrlRequest shortenUrlRequest) {
         try{
             ShortenedUrl shortenedUrl = new ShortenedUrl();
-            shortenedUrl.setShortUrl("shortened url");
             shortenedUrl.setLongUrl(shortenUrlRequest.getLongUrl());
+            shortenedUrl.setShortUrl(BASE_URL + generateRandomShortUrl());
             User user = userRepository.findUserByUsername(username);
             shortenedUrl.setUser(user);
             List<ShortenedUrl> shortenedUrls = user.getShortenedUrls();
@@ -39,4 +48,28 @@ public class UrlService {
         }
     }
 
+    public boolean checkShortLinkExists(String shortUrl) {
+        try{
+            ShortenedUrl shortenedUrl = urlsRepository.findByShortUrl(shortUrl);
+            return shortenedUrl != null;
+        }catch (Exception exception){
+            return true;
+        }
+    }
+
+    public String generateRandomShortUrl() {
+        char[] result = new char[NUM_CHARS_SHORT_LINK];
+        while (true) {
+            for (int i = 0; i < NUM_CHARS_SHORT_LINK; i++) {
+                int randomIndex = random.nextInt(ALPHABET.length() - 1);
+                result[i] = ALPHABET.charAt(randomIndex);
+            }
+            String shortUrl = new String(result);
+            // make sure the short link isn't already used
+            if (!checkShortLinkExists(shortUrl)) {
+                return shortUrl;
+            }
+        }
+    }
 }
+
