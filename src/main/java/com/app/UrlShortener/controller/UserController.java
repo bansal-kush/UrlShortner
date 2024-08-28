58554c548c66743c383c438d523f8d13a2b45516
kb150802 <bansalkushagra150802@gmail.com>
2024-08-28 21:24:24 +0530

    Cross origin

--- src/main/java/com/app/UrlShortener/controller/UserController.java
+++ src/main/java/com/app/UrlShortener/controller/UserController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin("http://localhost:3000")
 public class UserController {
     @Autowired
     UserService userService;
