be21b7d62ff3751e24dc000053b3eb6372a2bed4
kb150802 <bansalkushagra150802@gmail.com>
2024-11-15 21:06:30 +0530

    Revert "adding redis caching"
    This reverts commit eaf12563acd4def5dfc7523789d50567133bf98c.

--- src/main/java/com/app/UrlShortener/model/ShortenedUrl.java
+++ src/main/java/com/app/UrlShortener/model/ShortenedUrl.java
@@ -2,7 +2,9 @@ package com.app.UrlShortener.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
